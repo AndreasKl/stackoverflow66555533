e2877ca3e4089dc74d4c16b82fe585ba74ab1a96
Andreas Kluth <mail@andreaskluth.net>
2021-03-09 23:52:04 +0100

    Project setup.

---
+++ src/main/java/net/andreaskluth/mustachecustomformatter/MustacheCustomFormatterApplication.java
@@ -0,0 +1,13 @@
+package net.andreaskluth.mustachecustomformatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheCustomFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheCustomFormatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/andreaskluth/mustachecustomformatter/MustacheCustomFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.andreaskluth.mustachecustomformatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustacheCustomFormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
